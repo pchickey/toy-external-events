66baacfe1546346bb74639f519e7f8af82ee7cb4
Pat Hickey <p.hickey@f5.com>
2025-01-21 21:09:11 -0800

    enough to actually execute some wasm

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use bytes::Bytes;
 use std::collections::VecDeque;
 use std::future::Future;
 use std::pin::Pin;
+use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll, Waker};
 use wasmtime_wasi_io::poll::Pollable;
@@ -30,12 +31,22 @@ fn main() -> Result<()> {
     let instance_pre = linker.instantiate_pre(&component)?;
     let command_pre = toy_external_events::CommandPre::new(instance_pre)?;
 
-    async {
-        let mut store = Store::new(&engine, Ctx::new());
+    let clock = Clock::new();
+    let ctx = block_on(clock.clone(), async move {
+        let mut store = Store::new(&engine, Ctx::new(clock));
         let instance = command_pre.instantiate_async(&mut store).await?;
+        instance
+            .wasi_cli_run()
+            .call_run(&mut store)
+            .await?
+            .map_err(|()| anyhow!("instance exited with failure"))?;
+        Ok(store.into_data())
+    })?;
 
-        Ok::<_, anyhow::Error>(())
-    };
+    println!("stdout:");
+    ctx.stdout.report();
+    println!("stderr:");
+    ctx.stderr.report();
 
     Ok(())
 }
@@ -47,8 +58,7 @@ struct Ctx {
     stderr: TimestampedWrites,
 }
 impl Ctx {
-    fn new() -> Self {
-        let clock = Clock::new();
+    fn new(clock: Clock) -> Self {
         let stdout = TimestampedWrites::new(clock.clone());
         let stderr = TimestampedWrites::new(clock.clone());
 
@@ -85,32 +95,35 @@ impl toy_external_events::Embedding for Ctx {
 }
 
 #[derive(Debug, Clone)]
-struct Clock(Arc<Mutex<u64>>);
+pub struct Clock(Arc<AtomicU64>);
 impl Clock {
     pub fn new() -> Self {
-        Clock(Arc::new(Mutex::new(0)))
+        Clock(Arc::new(AtomicU64::new(0)))
     }
     pub fn get(&self) -> u64 {
-        *self.0.lock().unwrap()
+        self.0.load(Ordering::Relaxed)
+    }
+    fn increment(&self) {
+        self.0.fetch_add(1, Ordering::Relaxed);
     }
 }
 
 #[derive(Debug, Clone)]
-struct Deadline {
+pub struct Deadline {
     clock: Clock,
-    deadline: u64,
+    due: u64,
 }
 impl Deadline {
-    fn new(clock: Clock, deadline: u64) -> Self {
-        Self { clock, deadline }
+    fn new(clock: Clock, due: u64) -> Self {
+        Self { clock, due }
     }
 }
 impl Future for Deadline {
     type Output = ();
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let now = self.clock.get();
-        if now > self.deadline {
-            todo!("register waker with executor!!!");
+        if now > self.due {
+            Executor::current().push_deadline(self.clone(), cx.waker().clone());
             Poll::Pending
         } else {
             Poll::Ready(())
@@ -149,6 +162,11 @@ impl TimestampedWrites {
             log: Arc::new(Mutex::new(VecDeque::new())),
         }
     }
+    fn report(&self) {
+        for (time, line) in self.log.lock().unwrap().iter() {
+            println!("{:08} {:?}", time, String::from_utf8_lossy(line));
+        }
+    }
 }
 #[wasmtime_wasi_io::async_trait]
 impl Pollable for TimestampedWrites {
@@ -168,6 +186,81 @@ impl OutputStream for TimestampedWrites {
     }
 }
 
-struct Executor {
-    deadlines: Vec<(Deadline, std::task::Waker)>,
+static EXECUTOR: Mutex<Option<Executor>> = Mutex::new(None);
+
+pub struct Executor(Arc<Mutex<ExecutorInner>>);
+
+impl Executor {
+    pub fn current() -> Self {
+        Executor(
+            EXECUTOR
+                .lock()
+                .unwrap()
+                .as_ref()
+                .expect("Executor::current must be called within a running executor")
+                .0
+                .clone(),
+        )
+    }
+    pub fn push_deadline(&mut self, deadline: Deadline, waker: Waker) {
+        self.0.lock().unwrap().deadlines.push((deadline, waker))
+    }
+}
+
+pub fn block_on<R>(clock: Clock, f: impl Future<Output = Result<R>> + Send + 'static) -> Result<R> {
+    if EXECUTOR.lock().unwrap().is_some() {
+        panic!("cannot block_on while executor is running!")
+    }
+    let executor = Executor(Arc::new(Mutex::new(ExecutorInner {
+        deadlines: Vec::new(),
+    })));
+    *EXECUTOR.lock().unwrap() = Some(Executor(executor.0.clone()));
+
+    let waker = futures::task::noop_waker();
+    let mut cx = Context::from_waker(&waker);
+
+    let mut f = std::pin::pin!(f);
+    let r = 'outer: loop {
+        for _ in 0..200 {
+            match f.as_mut().poll(&mut cx) {
+                Poll::Pending => {}
+                Poll::Ready(r) => break 'outer r,
+            }
+        }
+        clock.increment();
+        for waker in executor.0.lock().unwrap().ready_deadlines(clock.get()) {
+            waker.wake()
+        }
+    };
+
+    let _ = EXECUTOR
+        .lock()
+        .unwrap()
+        .take()
+        .expect("executor vacated global while running");
+    r
+}
+
+struct ExecutorInner {
+    deadlines: Vec<(Deadline, Waker)>,
+}
+
+impl ExecutorInner {
+    fn ready_deadlines(&mut self, now: u64) -> Vec<Waker> {
+        let mut i = 0;
+        let mut wakers = Vec::new();
+        while i < self.deadlines.len() {
+            if let Some((deadline, _)) = self.deadlines.get(i) {
+                if deadline.due <= now {
+                    let (_, waker) = self.deadlines.remove(i);
+                    wakers.push(waker);
+                } else {
+                    i += 1;
+                }
+            } else {
+                break;
+            }
+        }
+        wakers
+    }
 }
